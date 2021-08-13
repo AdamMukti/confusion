c3c226a0daeaa2efbc55e1c49332a8f567d21f0c
Adam Mukti <adammukti08@gmail.com>
2021-08-13 21:35:33 +0700

    Intro. to Redux

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
 import "bootstrap-social/assets/css/font-awesome.css";
-import { BrowserRouter } from "react-router-dom";
 import Main from "./components/MainComponent";
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ConfigurationStore } from "./redux/configureStore";
 
+const store = ConfigurationStore();
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Main />
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
