52e4c1695e94a38d36039aad8b0b9425c6fd79d7
Adam Mukti <adammukti08@gmail.com>
2021-10-04 11:08:08 +0700

    Assignment 3

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,26 +1,21 @@
 import React, { Component } from "react";
 import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  Breadcrumb,
-  BreadcrumbItem,
   Button,
   Modal,
   ModalHeader,
   ModalBody,
+  Form,
+  FormGroup,
   Row,
+  Input,
   Label,
 } from "reactstrap";
-import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
+
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
 const minLength = (len) => (val) => val && val.length >= len;
-
 class CommentForm extends Component {
   constructor(props) {
     super(props);
@@ -86,16 +81,16 @@ class CommentForm extends Component {
                     maxLength: maxLength(15),
                   }}
                 />
-                <Errors
+                <Errors 
                   className="text-danger"
                   model=".author"
                   show="touched"
                   messages={{
-                    required: "Required",
-                    minLength: "Must be greater than 2 characters",
-                    maxLength: "Must be 15 characters or less",
+                    required: 'Required',
+                    minLength: 'Must be greater than 2 characters',
+                    maxLength: 'Must be 15 characters or less'
                   }}
-                />
+                  /> 
               </Row>
               <Row className="form-group px-3">
                 <Label htmlFor="comment">Comment</Label>
@@ -113,7 +108,7 @@ class CommentForm extends Component {
                 color="primary"
                 className="mt-3"
               >
-                Submit
+                Login
               </Button>
             </LocalForm>
           </ModalBody>
@@ -123,79 +118,4 @@ class CommentForm extends Component {
   }
 }
 
-function RenderDish({ dish }) {
-  if (dish != null) {
-    return (
-      <div className="col-md-5 col-12">
-        <Card>
-          <CardImg width="100%" src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      </div>
-    );
-  } else {
-    <div></div>;
-  }
-}
-
-function dateFormat(comment_date) {
-  const date = new Date(comment_date);
-  const month = date.toLocaleString("en-US", { month: "short" });
-  const day = date.toLocaleString("en-US", { day: "numeric" });
-  const year = date.toLocaleString("en-US", { year: "numeric" });
-
-  return `${month} ${day}, ${year}`;
-}
-
-function RenderComments({ comments }) {
-  if (comments != null) {
-    const comment = comments.map((c) => {
-      return (
-        <ul key={c.id} className="list-unstyled">
-          <li>{c.comment}</li>
-          <li>
-            -- {c.author} , {dateFormat(c.date)}
-          </li>
-        </ul>
-      );
-    });
-
-    return (
-      <div className="col-md-5 col-12">
-        <h4>Comments</h4>
-        {comment}
-        <CommentForm />
-      </div>
-    );
-  } else {
-    <div></div>;
-  }
-}
-
-const DishDetail = (props) => {
-  return (
-    <div className="container">
-      <div className="row">
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <Link to="/menu">Menu</Link>
-          </BreadcrumbItem>
-          <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-        </Breadcrumb>
-        <div className="col-12">
-          <h3>{props.dish.name}</h3>
-          <hr />
-        </div>
-      </div>
-      <div className="row mb-3">
-        <RenderDish dish={props.dish} />
-        <RenderComments comments={props.comments} />
-      </div>
-    </div>
-  );
-};
-
-export default DishDetail;
+export default CommentForm;
